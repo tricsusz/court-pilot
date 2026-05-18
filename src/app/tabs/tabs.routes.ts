e6a6949e4a0769df604c4921035a27cdd3070d85
Richard Toth <info@tothbt.com>
2026-05-18 19:06:52 +0200

    init Tauri

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,30 +7,27 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+        path: 'pilot',
+        loadComponent: () => import('../tab1/tab1.page').then((m) => m.Tab1Page)
       },
       {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+        path: 'umpires',
+        loadComponent: () => import('../tab2/tab2.page').then((m) => m.Tab2Page)
       },
       {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+        path: 'stats',
+        loadComponent: () => import('../tab3/tab3.page').then((m) => m.Tab3Page)
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full',
-      },
-    ],
+        redirectTo: '/tabs/pilot',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
-    pathMatch: 'full',
-  },
+    redirectTo: '/tabs/pilot',
+    pathMatch: 'full'
+  }
 ];
